d84c82e50b3ff54e36de5cd040298745bd6ca1eb
MaxwellAD <maxofwoodley@gmail.com>
2026-07-04 15:11:56 +0100

    Added user interface to calculator.cbl. Added some more cases, code coverage report is low due to the high useage of displays

--- Examples/Example02/test-calculator.cbl
+++ Examples/Example02/test-calculator.cbl
@@ -9,6 +9,12 @@
        COPY STORAGE.
        COPY CUTSTOR.
 
+       01 MOCKS.
+           05 MOCK-WS-INPUT PIC X(10).
+           05 MOCK-WS-NUM-1 PIC 9(9)v99.
+           05 MOCK-WS-NUM-2 PIC 9(9)v99.
+           05 MOCK-WS-RESULT PIC X(10). 
+
        PROCEDURE DIVISION.
        TEST-ADD-NUMBERS SECTION.
            *> GIVEN
@@ -154,8 +160,81 @@
            PERFORM CUT-END-TEST 
        .
 
+       TEST-ACCEPT-INPUT-DUAL SECTION.
+           *> TEST THE ABILITY FOR ACCEPT-INPUT TO TAKE THE DUAL INPUT
+           *> PATHWAY
+       
+           *> GIVEN
+           MOVE 'ADD' TO MOCK-WS-INPUT 
+
+           *> WHEN
+           PERFORM AD-ACCEPT-INPUT
+           
+           *> THEN
+           STRING 'AD-ACCEPT-INPUT '
+                  'FOLLOWED-BY AF-ACCEPT-NUM2'
+               DELIMITED BY SIZE
+               INTO CUT-TRACE 
+           END-STRING
+           PERFORM CUT-ASSERT-TRACE 
+       
+           PERFORM CUT-END-TEST 
+       .
+
+       TEST-ACCEPT-INPUT-SINGLE SECTION.
+           *> TEST THE AVILITY FOR ACCEPT-INPUT TO TAKE SINGL PATH
+       
+           *> GIVEN
+           MOVE 'SQRE' TO MOCK-WS-INPUT 
+           MOVE 5 TO MOCK-WS-NUM-2 
+       
+           *> WHEN
+           PERFORM AD-ACCEPT-INPUT 
+           
+           *> THEN
+           *> TODO IMPLEMENT NOT FOLLOWED-BY
+           STRING 'AD-ACCEPT-INPUT '
+                  'DIRECTLY-FOLLOWED-BY AG-ACCEPT-INPUT '
+                  'DIRECTLY-FOLLOWED-BY AE-ACCEPT-NUM1 '
+               DELIMITED BY SIZE
+               INTO CUT-TRACE 
+           END-STRING
+           PERFORM CUT-ASSERT-TRACE
+
+           
+       
+           PERFORM CUT-END-TEST 
+       .
+
+       TEST-INPUT-EVAL-ADD SECTION.
+           *> TEST THE INPUT EVALUATOR FOR ADD
+       
+           *> GIVEN
+           MOVE 'ADD' TO WS-INPUT 
+           MOVE 5 TO WS-NUM-1 
+           MOVE 6 TO WS-NUM-2 
+       
+           *> WHEN
+           PERFORM DA-EVALUATE-INPUT
+           
+           *> THEN
+           STRING 'DA-EVALUATE-INPUT '
+                  'FOLLOWED-BY BA-ADD-NUMBERS'
+               DELIMITED BY SIZE
+               INTO CUT-TRACE 
+           END-STRING
+           PERFORM CUT-ASSERT-TRACE
+
+           MOVE 11 TO CUT-ASSERT-TARGET-N 
+           MOVE WS-RESULT TO CUT-ASSERT-ACTUAL-N
+           PERFORM CUT-ASSERT-EQUALS-NUM
+
+           PERFORM CUT-END-TEST 
+       .
+
 
        END-TEST-SUITE SECTION.
+           PERFORM DISPLAY-COVERAGE 
            PERFORM CUT-END-TEST-SUITE
        .
        
@@ -188,6 +267,21 @@
                                                 CUT-TRACE-FIELD-INDEX
 
            EXIT SECTION
+       .
+
+       MOCK-AG-ACCEPT-INPUT SECTION.
+           MOVE MOCK-WS-INPUT TO WS-INPUT 
+           EXIT PARAGRAPH  
+       .
+
+       MOCK-AF-ACCEPT-NUM2 SECTION.
+           MOVE MOCK-WS-NUM-2 TO WS-NUM-2  
+           EXIT PARAGRAPH 
+       .
+
+       MOCK-AE-ACCEPT-NUM1 SECTION.
+           MOVE MOCK-WS-NUM-1 TO WS-NUM-1 
+           EXIT PARAGRAPH 
        .
 
       *****************************************************************
