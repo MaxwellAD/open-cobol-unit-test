459300dc0f542fb187b2ae4cb772ca7f7a24158a
MaxwellAD <maxofwoodley@gmail.com>
2026-08-16 18:49:22 +0100

    added sign to TARGET and ACTUAL fields

--- Examples/Example01/pgm-to-test.cbl
+++ Examples/Example01/pgm-to-test.cbl
@@ -150,8 +150,8 @@
        01 DUT-ASSERT-FIELDS.
            05 DUT-ASSERT-TARGET         PIC X(256)     VALUE SPACES. 
            05 DUT-ASSERT-ACTUAL         PIC X(256)     VALUE SPACES.
-           05 DUT-ASSERT-TARGET-N       PIC 9(18)V9(18).
-           05 DUT-ASSERT-ACTUAL-N       PIC 9(18)V9(18).
+           05 DUT-ASSERT-TARGET-N       PIC S9(18)V9(18).
+           05 DUT-ASSERT-ACTUAL-N       PIC S9(18)V9(18).
            *> Before displaying the TARGET and ACTUALS are moved to 
            *> THE DISPLAY-OUT mirrors
 
